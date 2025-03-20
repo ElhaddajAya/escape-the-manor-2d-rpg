d31b2d5135fb89670929485729edb3df479b02e9
ElhaddajAya <ayaelhaddaj619@gmail.com>
2025-03-20 10:18:23 +0000

    Added comments to scripts SpawnPointManager

--- Assets/SpawnPointManager.cs
+++ Assets/SpawnPointManager.cs
@@ -2,21 +2,23 @@ using UnityEngine;
 
 public static class SpawnPointManager
 {
-    private static string _targetSpawnPointName;
+    private static string _targetSpawnPointName; // Nom du spawn point cible
 
     public static void SetTargetSpawnPoint(string spawnPointName)
-    {
+    { // Stocker le nom du spawn point cible
         _targetSpawnPointName = spawnPointName;
     }
 
     public static Transform GetTargetSpawnPoint()
     {
+        // Vérifier si le nom du spawn point cible est défini
         if (string.IsNullOrEmpty(_targetSpawnPointName))
         {
             Debug.LogWarning("Aucun spawn point cible n'a été défini.");
             return null;
         }
 
+        // Trouver le spawn point cible dans la scène
         GameObject spawnPoint = GameObject.Find(_targetSpawnPointName);
         if (spawnPoint == null)
         {
@@ -24,6 +26,7 @@ public static class SpawnPointManager
             return null;
         }
 
+        // Retourner la position du spawn point
         return spawnPoint.transform;
     }
 }
