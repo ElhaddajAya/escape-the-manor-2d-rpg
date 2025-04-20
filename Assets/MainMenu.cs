9d1f049fb56b63d6b556ee91dba7cb8176ae482a
sally-ak <129871053+sally-ak@users.noreply.github.com>
2025-04-20 23:11:41 +0100

    Added quit button to main menu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -9,4 +9,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Main_Scene");
     }
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    } 
 }
