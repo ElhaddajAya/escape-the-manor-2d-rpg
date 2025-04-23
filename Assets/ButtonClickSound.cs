fc1dc9856d262122192662c5a4eaf9b31bfe7532
sally-ak <129871053+sally-ak@users.noreply.github.com>
2025-04-23 20:55:17 +0100

    Button sound fixation in progress

---
+++ Assets/ButtonClickSound.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class ButtonClickSound : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    void Start()
+    {
+        if (audioSource == null)
+            audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayPartialClip()
+    {
+        StartCoroutine(PlayShortSound());
+    }
+
+    private System.Collections.IEnumerator PlayShortSound()
+    {
+        audioSource.Play();
+        yield return new WaitForSeconds(0.5f); // play only 0.5s
+        audioSource.Stop();
+    }
+}
