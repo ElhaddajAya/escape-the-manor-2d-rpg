cac90f5527a7eaf5dcb2f20efc592a9ae82531b8
sally-ak <129871053+sally-ak@users.noreply.github.com>
2025-04-23 21:44:43 +0100

    Added fading in sound effect

--- Assets/SceneFader.cs
+++ Assets/SceneFader.cs
@@ -7,7 +7,7 @@ public class SceneFader : MonoBehaviour
 {
     public Image fadeImage;
     public float fadeDuration = 1f;
-
+    public AudioSource fadeSound;
     void Start()
     {
         // Start by fading in
@@ -35,18 +35,23 @@ public class SceneFader : MonoBehaviour
 
 
     IEnumerator FadeOut(string sceneName)
+{
+    // 🔊 Play the sound effect at the start of the fade
+    if (fadeSound != null)
+        fadeSound.Play();
+
+    float time = 0;
+    while (time < fadeDuration)
     {
-        float time = 0;
-        while (time < fadeDuration)
-        {
-            time += Time.deltaTime;
-            float alpha = time / fadeDuration;
-            SetAlpha(alpha);
-            yield return null;
-        }
-        SetAlpha(1);
-        SceneManager.LoadScene(sceneName);
+        time += Time.deltaTime;
+        float alpha = time / fadeDuration;
+        SetAlpha(alpha);
+        yield return null;
     }
+    SetAlpha(1);
+    SceneManager.LoadScene(sceneName);
+}
+
 
     void SetAlpha(float a)
     {
