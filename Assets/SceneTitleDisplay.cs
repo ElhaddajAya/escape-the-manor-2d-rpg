e82efe47c38352ddcb9a15c5ce876666616ac078
sally-ak <129871053+sally-ak@users.noreply.github.com>
2025-04-29 19:45:10 +0100

    Added fade in fade out effect to the title

--- Assets/SceneTitleDisplay.cs
+++ Assets/SceneTitleDisplay.cs
@@ -5,20 +5,51 @@ using System.Collections;
 public class SceneTitleDisplay : MonoBehaviour
 {
     public TextMeshProUGUI titleText;
-    public float displayTime = 3f;
+    public float fadeDuration = 1f;
+    public float displayTime = 3f; // Time fully visible before fade-out
 
     void Start()
     {
         if (titleText != null)
         {
-            StartCoroutine(ShowSceneTitle());
+            StartCoroutine(ShowTitleWithFade());
         }
     }
 
-    IEnumerator ShowSceneTitle()
+    IEnumerator ShowTitleWithFade()
     {
         titleText.gameObject.SetActive(true);
+
+        // Fade In
+        float t = 0;
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            float alpha = Mathf.Clamp01(t / fadeDuration);
+            SetAlpha(alpha);
+            yield return null;
+        }
+
+        // Wait while fully visible
         yield return new WaitForSeconds(displayTime);
+
+        // Fade Out
+        t = 0;
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            float alpha = Mathf.Clamp01(1 - (t / fadeDuration));
+            SetAlpha(alpha);
+            yield return null;
+        }
+
         titleText.gameObject.SetActive(false);
+    }
+
+    void SetAlpha(float a)
+    {
+        Color color = titleText.color;
+        color.a = a;
+        titleText.color = color;
     }
 }
